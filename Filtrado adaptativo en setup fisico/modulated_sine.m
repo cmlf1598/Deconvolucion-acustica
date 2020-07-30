bf15f35dde5565fec3ade334db993f7a8c504014
Carlos López <60441989+cmlf1598@users.noreply.github.com>
2020-07-30 10:17:14 -0600

    Función que genera señales AM y FM

---
+++ Filtrado adaptativo en setup fisico/modulated_sine.m
@@ -0,0 +1,30 @@
+%%Modulated sine
+%Por Carlos Manuel López
+%30-7-20
+
+%Función para generar una señal sinusoidal modulada
+%Inputs:
+    % fs - frecuencia de muestreo
+    % tf - duración deseada
+    % fc - frecuencia central (Hz)
+    % f_low- frecuencia del oscilador de baja frecuencia
+    % Ac - amplitud central 
+    % mod - tipo de modulación, AM o FM
+%Outputs:
+    % Y - señal sinusoidal modulada
+    % t - eje temporal
+
+function [Y, t] = modulated_sine(fs, tf, fc, Ac, f_low, mod)
+    dt = 1/fs;
+    
+    phi = 0;
+    t = 0:dt:(tf - dt);
+    LFO = sin(2*pi*f_low*t);
+    switch mod
+        case 'AM'
+            Y = LFO.*(Ac*sin( (2*pi*fc*t) + (phi)*(pi/180) ));
+        case 'FM'
+            Y = (Ac*sin( (2*pi*fc*t) + (phi + LFO*90)*(pi/180) ));
+    end
+end
+
